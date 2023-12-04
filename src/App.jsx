6c0567805b2160ae19af58e47a5346a8a8192e7d
Kuzma02 <aleksandarkuzmanovic02@gmail.com>
2023-12-04 19:38:15 +0100

    “first_push”

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <section className="testimonials">
+      <div className="container">
+        <div className="items item1">ITEM</div>
+        <div className="items item2">ITEM</div>
+        <div className="items item3">ITEM</div>
+        <div className="items item4">ITEM</div>
+        <div className="items item5">ITEM</div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
